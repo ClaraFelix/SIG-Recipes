a50d50ff8dcfbc5f11fe96ca30603608c0d58c59
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2021-11-08 19:55:03 -0300

    Reorganizando estrutura do projeto

--- preparo.c
+++
@@ -1,4 +0,0 @@
-//Modo de preparo
-quantidade_ingredientes = 0;
-char tipos_ingredientes;
-int tempo_forno;
--- recipes.c
+++ recipes.c
@@ -11,101 +11,50 @@
 #include <stdlib.h>
 #include<string.h>
 
-//Teste tela de cadastrar receitas (testando o compilador)
-
-void main(void){
-    void telaCadastrarReceitas(void);
-
-    char nome_receita[20];
-
-    struct listar_ingredientes{
-        char nome[20];
-        int quantidade_ing;
-        char ing[100];
-    };
-
-//Solicita ao usuário que cadastre uma receita
-struct lista_ingredientes receita[1];
-
-for(int x=0;x<1;x++){                          
-        printf("Digite o nome da receita que deseja cadstrar: ");
-        scanf("%20[^\n]",&receita[x].nome); //O nome da receita deverá contar menos que 20 caracteres
-        fflush(stdin);
-        printf("Quantos ingredientes essa receita é composta? ");
-        scanf("%i",&receita[x].quantidade_ing);
-        fflush(stdin);
-
-        //Armazena os ingredientes que o usuário irá digitar, baseado na quantidade de ingredientes acima 
-
-        char ingredientes[receita[x].quantidade_ing][20];
-            for(int y=0;y<receita[x].quantidade_ing;y++){
-                printf("Digite o ingrediente %i da receita %i: ",y+1,x+1);
-                scanf("%20[^\n]",&ingredientes[y]);
-                fflush(stdin);
-            }
-
-}
-
-    
-
-
-
-/////
 // Assinatura das funções
-
-/*void telaSobreSIG(void);
+void telaSobreSIG(void);
 void telaMain(void);
-void telaEquipe(void);
 void telaMenuReceitas(void);
 void telaCadastrarReceitas(void);
 void telaBuscarReceita(void);
 void telaEditarReceita(void);
 void telaExcluirReceita(void);
 
-
-
-/////
 // Programa principal
+int main(void) {
+   telaSobreSIG();
+   telaMenuReceitas();
+   telaCadastrarReceitas();
+   telaBuscarReceita();
+   telaEditarReceita();
+   telaExcluirReceita();
+   return 0;
+}
 
-//int main(void) {
-    telaSobreSIG();
-    telaMain();
-    telaEquipe();
-    telaMenuReceitas();
-    telaCadastrarReceitas();
-    telaBuscarReceita();
-    telaEditarReceita();
-    telaExcluirReceita();
-    return 0;
-}*/
-
-
-
-/////
 // Funções
 
-/*void telaSobreSIG(void) {
-    system("clear||cls");
+void telaSobreSIG(void) {
+    system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                            ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                    ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                         ///\n");
-    printf("///               Departamento de Computação e Tecnologia                       ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                          ///\n");
-    printf("///         SIG-Recipes: Um Caderno Virtual de Receitas Culinárias              ///\n");
-    printf("///                Developed by  @clarafelix - Out, 2021                        ///\n");
-    printf("///                                                                            ///\n");
-    printf("//////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                            ///\n");
-    printf("///                    = = = = = SIG-Recipes = = = = =                         ///\n");
-    printf("///                                                                            ///\n");
-    printf("/// Projeto desenvolvido para a disciplina de Programação, curso de            ///\n");
-    printf("/// Sistemas de Informação/UFRN. Consiste na elaboração de uma caderno virtual ///\n");
-    printf("/// para armazenamento de receitas e posterior consultas. Tem por objetivo o   ///\n");
-    printf("/// desenvolvimento de um programa/projeto, utilizando-se da linguagem C, onde o///\n");
-    printf("/// usuário poderá cadastrar e armazenar suas receitas, de forma semelhante a ///\n");
-    printf("/// um caderno de anotações e/ou bloco de notas.                              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///         SIG-Recipes: Um Caderno Virtual de Receitas Culinárias          ///\n");
+    printf("///                Developed by  @clarafelix - Out, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                    = = = = = SIG-Recipes = = = = =                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("/// Projeto desenvolvido para a disciplina de Programação, curso de         ///\n");
+    printf("/// Sistemas de Informação/UFRN. Consiste na elaboração de uma caderno vir- ///\n");
+    printf("/// tual para armazenamento de receitas e posterior consultas. Tem por      ///\n");
+    printf("/// objetivo o desenvolvimento de um programa/projeto, utilizando-se da     ///\n");
+    printf("/// linguagem C, onde o usuário poderá cadastrar e armazenar suas receitas, ///\n"); 
+    printf("///  de forma semelhante a um caderno de anotações e/ou bloco de notas.     ///\n");
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -116,7 +65,7 @@ void telaExcluirReceita(void);
 
 
 
-void telaMain(void) {
+/*void telaMain(void) {
     system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
