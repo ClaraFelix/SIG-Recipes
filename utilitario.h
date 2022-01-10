c3367a79592c8e3b14110a20c2a849ae6cfbe342
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2022-01-10 17:50:45 -0300

    Desenvolvimento do módulo utilitários

---
+++ utilitario.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///          SIG-Recipes: Um Caderno Virtual de Receitas Culinárias         ///
+///                Developed by  @clarafelix - Out, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+//Assinaturas das funções do módulo utilitários
+
+void clear (void);
+int validarCPF(char);
